057a892e56431cef121aedbf84789e0f42ba3070
aryanahlag <aryanahla6@gmail.com>
2022-01-04 11:33:49 +0700

    update post&delete api (not complete when push data to data object)

--- src/app/api-test/api-test.component.ts
+++ src/app/api-test/api-test.component.ts
@@ -1,13 +1,47 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {MatTable} from "@angular/material/table";
 
 export class config {
   constructor(
     public id: number,
     public userId: string,
-    public title: string
+    public title: string,
+    public body: string
   ) {
   }
+}
+
+@Component({
+  selector: 'api-test-dialog',
+  templateUrl: 'api-test-dialog.component.html'
+})
+
+export class ApiTestDialogComponent implements OnInit {
+  formValue: FormGroup;
+
+  constructor(private httpClient: HttpClient, private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit():void {
+    this.formValue = this.formBuilder.group({
+      title: '',
+      body: '',
+      userId: ''
+    })
+  }
+
+  postDataAPI() {
+    this.httpClient.post<any>('https://jsonplaceholder.typicode.com/posts', this.formValue.value, {headers: {'Content-type': 'application/json; charset=UTF-8'}})
+      .subscribe(res => {
+        console.log('data: ',res)
+      }, err => {
+        console.log(err)
+      })
+  }
+
 }
 
 @Component({
@@ -15,22 +49,49 @@ export class config {
   templateUrl: './api-test.component.html',
   styleUrls: ['./api-test.component.scss']
 })
+
 export class ApiTestComponent implements OnInit {
 
-  data: config[];
+  displayedColumns: string[] = ['id', 'title', 'body', 'userId', 'delete'];
 
-  constructor(private httpClient: HttpClient) {
+  data: config[];
+  @ViewChild(MatTable) table: MatTable<any>
+
+  constructor(private httpClient: HttpClient, public dialog: MatDialog) {
   }
 
   ngOnInit(): void {
     this.getDataAPI()
   }
 
+  openDialog() {
+    const dialogConfig = new MatDialogConfig()
+    const dialogRef = this.dialog.open(ApiTestDialogComponent);
+
+    dialogConfig.autoFocus = true
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
   getDataAPI() {
-    this.httpClient.get<any>('https://jsonplaceholder.typicode.com/todos').subscribe(res => {
+    this.httpClient.get<any>('https://jsonplaceholder.typicode.com/posts').subscribe(res => {
       console.log("response: ", res)
       this.data = res
     })
+  }
+
+  deleteDataAPI(dataElement:any,index:any):void{
+    this.httpClient.delete<any>('https://jsonplaceholder.typicode.com/posts/'+dataElement.id)
+      .subscribe(res => {
+        this.data.splice(index,1)
+        this.table.renderRows()
+        alert('success delete')
+      }, err => {
+        console.log(err)
+        alert('failed to process')
+      })
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatTableModule} from '@angular/material/table';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EdgeClicksComponent } from './edge-clicks/edge-clicks.component';
-import { ApiTestComponent } from './api-test/api-test.component';
+import { ApiTestComponent, ApiTestDialogComponent } from './api-test/api-test.component';
 import {HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     EdgeClicksComponent,
-    ApiTestComponent
+    ApiTestComponent,
+    ApiTestDialogComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
